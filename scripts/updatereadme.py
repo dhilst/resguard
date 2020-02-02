8e1cd2c6a2e35424f401db05b7d3e3ff0852cf64
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-02-02 18:29:54 -0300

    Add build shild to updatereadme script

--- scripts/updatereadme.py
+++ scripts/updatereadme.py
@@ -2,9 +2,15 @@
 import sys
 import os
 from textwrap import dedent
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(BASE_PATH)
 import resguard
+from subprocess import check_output
 
+branch = check_output(f"git -C {BASE_PATH} rev-parse --abbrev-ref HEAD", shell=True, universal_newlines=True).rsplit()
+
+
+print("[![Build Status](https://travis-ci.org/dhilst/resguard.svg?branch={branch})](https://travis-ci.org/dhilst/resguard)")
 print(resguard.__doc__)
 print()
 for func in 'parse_dc create_base unpack_union Dataclass'.split():
