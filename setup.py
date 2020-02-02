e7f331a24745e45a38444c8c27ab82a273334f00
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-02-02 18:38:15 -0300

    Bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="resguard",
     author="Daniel Hilst Selli",
     author_email="danielhilst@gmail.com",
-    version="0.1",
+    version="0.2",
     py_modules=["resguard"],
     python_requires=">=3.6",
     long_description=README,
